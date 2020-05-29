7d013da6eaa35b7c5dbcbfd1dd8dd6bdca51a93a
Monji0502 <shizuto1031@gmail.com>
2020-05-29 14:50:29 +0900

    Initial Commit

---
+++ Honeycomb_English/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeycomb_English
+//
+//  Created by 中谷静斗 on 2020/05/29.
+//  Copyright © 2020 中谷静斗. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
